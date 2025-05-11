00b4ea3c61a1b84b6f727ccbf7e27f70c97bbd34
sharqawycs <sharqawycs@gmail.com>
2025-05-11 03:27:48 +0300

    Update DotsPuzzleSolver.java

--- src/main/java/asu/caie/algorithmproject/tasks/tasksix/algorithms/DotsPuzzleSolver.java
+++ src/main/java/asu/caie/algorithmproject/tasks/tasksix/algorithms/DotsPuzzleSolver.java
@@ -1,18 +1,17 @@
 package asu.caie.algorithmproject.tasks.tasksix.algorithms;
 
-import asu.caie.algorithmproject.tasks.tasksix.views.PathVisualizer;
+import  asu.caie.algorithmproject.tasks.tasksix.models.Point;
 
-import javax.swing.*;
-import java.awt.*;
+
 import java.util.*;
 import java.util.List;
 
 public class DotsPuzzleSolver {
-	private static LinkedList<asu.caie.algorithmproject.tasks.tasksix.models.Point> path = new LinkedList<asu.caie.algorithmproject.tasks.tasksix.models.Point>();
-	private static Map<Integer, List<asu.caie.algorithmproject.tasks.tasksix.models.Point>> dp = new HashMap<>();
+	private static LinkedList<Point> path = new LinkedList<Point>();
+	private static Map<Integer, List<Point>> dp = new HashMap<>();
 
 	// DP SOLUTION
-	public static LinkedList<asu.caie.algorithmproject.tasks.tasksix.models.Point> solveWithDP(int n) {
+	public static LinkedList<Point> solveWithDP(int n) {
 		initializeDPTable();
 
 		// DP
@@ -28,59 +27,59 @@ public class DotsPuzzleSolver {
 
 	private static void initializeDPTable() {
 		// n = 3
-		List<asu.caie.algorithmproject.tasks.tasksix.models.Point> base3 = new ArrayList<>();
-		base3.add(new asu.caie.algorithmproject.tasks.tasksix.models.Point(0, 3));
-		base3.add(new asu.caie.algorithmproject.tasks.tasksix.models.Point(3, 3));
-		base3.add(new asu.caie.algorithmproject.tasks.tasksix.models.Point(0, 0));
-		base3.add(new asu.caie.algorithmproject.tasks.tasksix.models.Point(0, 3));
-		base3.add(new asu.caie.algorithmproject.tasks.tasksix.models.Point(2, 1));
+		List<Point> base3 = new ArrayList<>();
+		base3.add(new Point(0, 3));
+		base3.add(new Point(3, 3));
+		base3.add(new Point(0, 0));
+		base3.add(new Point(0, 3));
+		base3.add(new Point(2, 1));
 		dp.put(3, base3);
 	}
 
 	private static void buildSolutionForSize(int n) {
-		List<asu.caie.algorithmproject.tasks.tasksix.models.Point> path = new ArrayList<>();
-		asu.caie.algorithmproject.tasks.tasksix.models.Point startPoint = getStartPoint(n);
+		List<Point> path = new ArrayList<>();
+		Point startPoint = getStartPoint(n);
 		path.add(startPoint);
 
-		List<asu.caie.algorithmproject.tasks.tasksix.models.Point> prevPath = dp.get(n - 1);
-		asu.caie.algorithmproject.tasks.tasksix.models.Point lastPoint = prevPath.get(prevPath.size() - 1);
+		List<Point> prevPath = dp.get(n - 1);
+		Point lastPoint = prevPath.get(prevPath.size() - 1);
 
 		if (isEven(n)) {
-			path.add(new asu.caie.algorithmproject.tasks.tasksix.models.Point(startPoint.x - n + 1, startPoint.y));
-			path.add(new asu.caie.algorithmproject.tasks.tasksix.models.Point(startPoint.x - n + 1, startPoint.y + n - 1));
+			path.add(new Point(startPoint.x - n + 1, startPoint.y));
+			path.add(new Point(startPoint.x - n + 1, startPoint.y + n - 1));
 
 			int offsetX = startPoint.x - n + 2 - prevPath.get(0).x;
 			int offsetY = startPoint.y + n - 1 - prevPath.get(0).y;
 
 			for (int i = 1; i < prevPath.size(); i++) {
-				asu.caie.algorithmproject.tasks.tasksix.models.Point p = prevPath.get(i);
-				path.add(new asu.caie.algorithmproject.tasks.tasksix.models.Point(p.x + offsetX, p.y + offsetY));
+				Point p = prevPath.get(i);
+				path.add(new Point(p.x + offsetX, p.y + offsetY));
 			}
 		} else {
-			path.add(new asu.caie.algorithmproject.tasks.tasksix.models.Point(startPoint.x + n - 1, startPoint.y));
-			path.add(new asu.caie.algorithmproject.tasks.tasksix.models.Point(startPoint.x + n - 1, startPoint.y - n + 1));
+			path.add(new Point(startPoint.x + n - 1, startPoint.y));
+			path.add(new Point(startPoint.x + n - 1, startPoint.y - n + 1));
 
 			int offsetX = startPoint.x + n - 2 - prevPath.get(0).x;
 			int offsetY = startPoint.y - n + 1 - prevPath.get(0).y;
 
 			for (int i = 1; i < prevPath.size(); i++) {
-				asu.caie.algorithmproject.tasks.tasksix.models.Point p = prevPath.get(i);
-				path.add(new asu.caie.algorithmproject.tasks.tasksix.models.Point(p.x + offsetX, p.y + offsetY));
+				Point p = prevPath.get(i);
+				path.add(new Point(p.x + offsetX, p.y + offsetY));
 			}
 		}
 
 		dp.put(n, path);
 	}
 
-	private static asu.caie.algorithmproject.tasks.tasksix.models.Point getStartPoint(int n) {
+	private static Point getStartPoint(int n) {
 		if (n == 3) {
-			return new asu.caie.algorithmproject.tasks.tasksix.models.Point(0, n);
+			return new Point(0, n);
 		}
 
 		if (isEven(n)) { // Bottom Right
-			return new asu.caie.algorithmproject.tasks.tasksix.models.Point(n - 1, 0);
+			return new Point(n - 1, 0);
 		} else { // Top Left
-			return new asu.caie.algorithmproject.tasks.tasksix.models.Point(0, n - 1);
+			return new Point(0, n - 1);
 		}
 	}
 
